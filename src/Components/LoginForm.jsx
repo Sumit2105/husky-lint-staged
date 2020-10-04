61d9c8675d4cab57718a23e426164dd54c8c88eb
sumit-tal <sumit.sarkar@talentica.com>
2020-10-04 17:46:45 +0530

    added .rc files

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,17 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 
 const LoginForm = (props) => {
   const [userName, setUserName] = useState(null);
   const [password, setPassword] = useState(null);
 
   const onSubmit = () => {
-    console.log("userName", userName);
-    console.log("password", password);
+    console.log('userName', userName);
+    console.log('password', password);
   };
 
   return (
     <div>
-      <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)} />
+      <input
+        type="text"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
       <input
         type="password"
         value={password}
